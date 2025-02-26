7aa85eb19898fc0fa641fea8b4813a2dca727ff8
Priyanka-Jammu <priyankajammu1@gmail.com>
2025-02-26 10:17:18 -0500

    newtext

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World Class!");
+        Console.ReadLine();
+
+        int x;
+        x=3;
+    }
+}
